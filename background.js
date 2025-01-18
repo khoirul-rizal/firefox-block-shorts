acbcb2bd9308ee3f96397ff09ef095436c6338fd
M Khoirul Rizal <khotech47@gmail.com>
2025-01-18 15:55:25 +0700

    fix: disable block short, specify tracking url

--- background.js
+++ background.js
@@ -4,8 +4,25 @@ const urlPatterns = [
   /^https?:\/\/(www\.)?facebook\.com\/reel\/.*/i
 ];
 
-browser.tabs.onUpdated.addListener((listener, filter) => {
-if (filter.url && urlPatterns.some(pattern => pattern.test(filter.url))) {
-    browser.tabs.update(listener, { url: "https://google.com" });
+let isEnable = true
+
+const redirectShorts = (listener, filter) => {
+  if ( isEnable && filter.url && urlPatterns.some(pattern => pattern.test(filter.url))) {
+      browser.tabs.update(listener, { url: "https://google.com" });
+    }
+}
+
+  browser.tabs.onUpdated.addListener(redirectShorts);
+
+browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === 'toggleBlock') {
+      isEnable = !isEnable
+      sendResponse({isEnable})
   }
-});
+
+  if (message.action === 'status') {
+      sendResponse({isEnable})
+  }
+})
+
+
--- popup.js
+++ popup.js
@@ -1,3 +1,40 @@
-document.getElementById('myButton').addEventListener('click', () => {
-  console.log('HITTED');
+const toggleBtn = document.getElementById('btn_disable')
+const pswdLabel = document.getElementById('pswd_label')
+const pswdInput = document.getElementById('pswd_input')
+
+const changeState = (isEnable) => {
+  toggleBtn.textContent = isEnable ? 'Disable Block' : 'Enable Block'
+  if (isEnable) {
+    pswdLabel.style.display = 'unset'
+    pswdInput.style.display = 'unset'
+  } else {
+    pswdLabel.style.display = 'none'
+    pswdInput.style.display = 'none'
+  }
+}
+
+
+browser.runtime.sendMessage({action: 'status'}).then(resp => {
+  console.log('!dev! init', resp)
+  const isEnable = resp.isEnable
+  changeState(isEnable)
+})
+
+
+document.getElementById('btn_disable').addEventListener('click', async() => {
+  const status = (await browser.runtime.sendMessage({action: 'status'})).isEnable
+  if (status) {
+    if (pswdInput.value === 'I WANT MY BRAIN TO BE BRAIN ROTTED') {
+      const resp = await browser.runtime.sendMessage({action: 'toggleBlock'})
+      changeState(resp.isEnable)
+    } else {
+      // console.log('falseee', pswdInput)
+      // pswdInput.focus()
+      // pswdInput.value === 'ADMIT IT !!!!!'
+      // pswdInput.select()
+    }
+  } else {
+    const resp = await browser.runtime.sendMessage({action: 'toggleBlock'})
+    changeState(resp.isEnable)
+  }
 });
